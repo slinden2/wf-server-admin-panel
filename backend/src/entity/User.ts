9971e61786869bd9f3de7cc1a678c82dfd8e29f7
Samu <samu.linden@gmail.com>
2020-11-10 17:53:48 +0100

    Adds servers field to user entity

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -41,4 +41,35 @@ export class User extends BaseEntity {
 
   @OneToMany(() => ServerUser, (su) => su.user)
   serverConnection: ServerUser[];
+
+  @Field(() => [Server])
+  async servers(@Ctx() ctx: Context): Promise<Server[]> {
+    // Admin gets to see all servers
+    if (ctx.session?.role === "ADMIN") {
+      return await Server.find();
+    }
+
+    // USER gets to see only appointed servers
+    const serverUsers = await ServerUser.find({
+      join: {
+        alias: "serverUsers",
+        innerJoinAndSelect: {
+          server: "serverUsers.server",
+        },
+      },
+      where: {
+        userId: this.id,
+      },
+    });
+
+    const servers: Server[] = serverUsers.map((su) => {
+      const newServer = new Server();
+      newServer.id = su.server.id;
+      newServer.name = su.server.name;
+      newServer.pid = su.server.pid;
+      return newServer;
+    });
+
+    return servers;
+  }
 }
