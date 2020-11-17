05c180495c419abdea350948f09cad8ae8aa6ae6
Samu <samu.linden@gmail.com>
2020-11-17 11:40:51 +0100

    Migrates graphql files form .ts to .graphql extension

--- frontend/src/graphql/mutations/server/setUserServers.ts
+++
@@ -1,6 +0,0 @@
-import { gql } from "@apollo/client";
-
-export const setUserServersMutation = gql`
-  mutation SetUserServers($userId: String!, $serverIds: [String!]!) {
-    setUserServers(data: { userId: $userId, serverIds: $serverIds })
-`;
