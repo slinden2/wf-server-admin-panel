5262a3d808491f247b99bbce4ca1fb18073aa90d
Samu Linden <samu.linden@gmail.com>
2020-11-27 22:08:01 +0100

    Adds Labbe ping to the webhook

--- backend/src/webhook/sendMessageWebhook.ts
+++ backend/src/webhook/sendMessageWebhook.ts
@@ -3,6 +3,6 @@ import config from "../config";
 
 export const sendMessageWebhook = async (id: string, username: string) => {
   await axios.post(config.discord.webhookUri, {
-    content: `New user created. Id: ${id}, username: ${username}`,
+    content: `<@104982775492276224> New user created. Id: ${id}, username: ${username}`,
   });
 };
